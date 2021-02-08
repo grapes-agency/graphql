7ebe9d8071570fa66c345e1201bc3d84c4a973dc
Torsten <info@by-torsten.com>
2021-02-08 14:13:12 +0100

    fix(apollo-link-local-federation): Return null on unresolvable path

--- packages/apollo-link-local-federation/src/utils/getWithPath.ts
+++ packages/apollo-link-local-federation/src/utils/getWithPath.ts
@@ -3,7 +3,7 @@ interface PathObj {
   path: string
 }
 
-export const getWithPath = (data: Array<Record<string, any>> | Record<string, any>, path: string) => {
+export const getWithPath = (data: Array<Record<string, any>> | Record<string, any> | null, path: string) => {
   if (Array.isArray(data)) {
     const operationData: Array<PathObj> = []
     data.forEach((subData, index) =>
@@ -17,6 +17,10 @@ export const getWithPath = (data: Array<Record<string, any>> | Record<string, an
     return operationData
   }
 
+  if (!data) {
+    return []
+  }
+
   const segments = path.split('.')
   const currentPath: Array<string> = []
   let currentData = data
@@ -25,6 +29,10 @@ export const getWithPath = (data: Array<Record<string, any>> | Record<string, an
   for (const segment of segments) {
     currentPath.push(segment)
     currentData = currentData[segment]
+
+    if (currentData === null) {
+      break
+    }
 
     if (Array.isArray(currentData) && currentPath.length < segments.length) {
       const pathLeft = segments.slice(currentPath.length).join('.')
