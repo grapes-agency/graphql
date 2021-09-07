ef03d919c3014a253d43ffdd7da0d75578cb9e92
Torsten <info@by-torsten.com>
2021-09-07 14:21:03 +0200

    fix(apollo-link-webworker): Clean up context store

--- packages/apollo-link-webworker/src/RemoteOperation.ts
+++ packages/apollo-link-webworker/src/RemoteOperation.ts
@@ -7,14 +7,18 @@ const operationStorage = new Map<string, Operation>()
 const operationTransferHandler: Comlink.TransferHandler<Operation, Omit<Operation, 'setContext' | 'getContext'>> = {
   canHandle: (obj): obj is Operation => typeof obj === 'object' && obj !== null && 'operationName' in obj,
   serialize: operation => {
-    operationStorage.set(operation.operationName, operation)
+    if (operation.operationName) {
+      operationStorage.set(operation.operationName, operation)
+    }
     const { setContext, getContext, ...transferableOperation } = operation
 
     return [transferableOperation, []]
   },
   deserialize: transferableOperation => {
-    if (operationStorage.has(transferableOperation.operationName)) {
+    if (transferableOperation.operationName && operationStorage.has(transferableOperation.operationName)) {
       const operation = operationStorage.get(transferableOperation.operationName)!
+      operationStorage.delete(transferableOperation.operationName)
+
       return createOperation(operation.getContext(), transferableOperation)
     }
 
